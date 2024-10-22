d7f60cda966ede8bfd8789f3946eb14dbe88f95c
Urooj Baloch <144048378+Urooj-Baloch@users.noreply.github.com>
2024-10-22 10:12:56 +0500

    Add files via upload

---
+++ Labs/Lab 8/TASK 2.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+class Character{
+ protected:
+  int health;
+  int damage;
+ public:
+  Character(int health, int damage): health(health), damage(damage)
+  {}
+  
+  void display()
+  {
+   cout<<"Health: "<<health<<endl;
+   cout<<"Damage: "<<damage<<endl; }
+   
+  
+};
+class Enemy:public Character{
+ public:
+  Enemy(int health, int damage):Character(health, damage)
+  {
+  }
+  void display()
+  {
+   cout<<"Health: "<<health<<endl;
+   cout<<"Damage: "<<damage<<endl; }
+};  
+	
+class Player:public Character{
+ public:
+  Player(int health, int damage):Character(health, damage)
+  {
+  }
+  void display()
+  {
+   cout<<"Health: "<<health<<endl;
+   cout<<"Damage: "<<damage<<endl; }
+}; 
+
+class Wizard:protected Player{
+ private:
+  int magicPower;
+  string spells;
+  
+ public:
+  Wizard(int magicPower, string spells, int health, int damage):Player(health, damage), magicPower(magicPower), spells(spells)
+  {
+  }
+  
+  void display()
+  {
+   cout<<"------------Displaying the information for wizard---------"<<endl;
+   cout<<"Health: "<<health<<endl;
+   cout<<"Damage: "<<damage<<endl; 
+   cout<<"Magic Power: "<<magicPower<<endl;
+   cout<<"Spells: "<<spells<<endl;
+  }
+}; 
+int main()
+{
+ Wizard w1(3, "shaka laka boom",90, 10);
+ w1.display();
+}
+#
